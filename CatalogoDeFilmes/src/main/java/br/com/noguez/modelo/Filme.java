4d85150dfcd729972e546087e5f5ffa7975a38f5
Felipe-Noguez <felipenoguez@outlook.com.br>
2022-02-15 12:09:51 -0300

    conteúdo da aula 1

---
+++ CatalogoDeFilmes/src/main/java/br/com/noguez/modelo/Filme.java
@@ -0,0 +1,26 @@
+package br.com.noguez.modelo;
+
+public class Filme {
+	
+	private String nome;
+	private String genero;
+	private int ano;
+	
+	public Filme(String nome, String genero, int ano) {
+		this.nome = nome;
+		this.genero = genero;
+		this.ano = ano;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+}
